13d23d002dfba0ed2185a65709fcffc61d4a7781
Hampussss <hampus.skjonsbergbamegard@elev.ga.lbs.se>
2024-11-06 10:19:48 +0100

    Weapon Spawn/Despawn

--- SniperMonkeys/Assets/Code/PlayerCode/EnemyDamager.cs
+++ SniperMonkeys/Assets/Code/PlayerCode/EnemyDamager.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class EnemyDamager : MonoBehaviour
 {
     public float DamageAmount;
+    public float LifeTime, GrowSpeed = 4f;
+    private Vector3 TargetSize;
+    public bool DestroyParent;
 
 
     void Start()
     {
-        
+        //Destroy(gameObject, LifeTime);
+
+        TargetSize = transform.localScale;
+        transform.localScale = Vector3.zero;
     }
 
     void Update()
     {
-        
+        transform.localScale = Vector3.MoveTowards(transform.localScale, TargetSize, GrowSpeed * Time.deltaTime);
+
+        LifeTime -= Time.deltaTime;
+
+        if(LifeTime <= 0)
+        {
+            TargetSize = Vector3.zero;
+            if(transform.localScale.x == 0f)
+            {
+                Destroy(gameObject);
+
+                if (DestroyParent)
+                {
+                    Destroy(transform.parent.gameObject);
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
--- SniperMonkeys/Assets/Code/PlayerCode/PlayerWeapons/LargeSword.cs
+++ SniperMonkeys/Assets/Code/PlayerCode/PlayerWeapons/LargeSword.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class LargeSword : MonoBehaviour
 {
     public float RotateSpeed;
-    public Transform holder;
+    public Transform holder, SwordToSpawn;
+    public float TimeBetweenSpawn;
+    private float SpawnCounter;
 
     void Start()
     {
@@ -15,5 +17,14 @@ public class LargeSword : MonoBehaviour
     void Update()
     {
         holder.rotation = Quaternion.Euler(0f, 0f, holder.rotation.eulerAngles.z + (RotateSpeed * Time.deltaTime));
+
+        SpawnCounter -= Time.deltaTime;
+
+        if(SpawnCounter <= 0)
+        {
+            SpawnCounter = TimeBetweenSpawn;
+
+            Instantiate(SwordToSpawn, SwordToSpawn.position, SwordToSpawn.rotation, holder).gameObject.SetActive(true);
+        }
     }
 }
